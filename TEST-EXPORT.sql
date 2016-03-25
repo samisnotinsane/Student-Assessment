73af4c397862d2e4829575da4acfa07264f67b16
Tom Jagielski <ec14421@qmul.ac.uk>
2016-03-25 17:50:06 +0000

    Added TEST-EXPORT.sql to see what final script should look like

---
+++ TEST-EXPORT.sql
@@ -0,0 +1,14 @@
+--
+-- File generated with SQLiteStudio v3.0.7 on Fr Mrz 25 17:48:51 2016
+--
+-- Text encoding used: windows-1252
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: STUDENT
+DROP TABLE IF EXISTS STUDENT;
+CREATE TABLE STUDENT (STUDENT_ID INTEGER (10) PRIMARY KEY NOT NULL);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
