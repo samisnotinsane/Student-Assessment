a839b88fbe0ad09b8977d4058ca3b74df1a218a6
Sameen Islam <ec14370@qmul.ac.uk>
2016-04-26 17:43:04 +0100

    Added sameen's 3rd script

---
+++ QUERY/StudentPassedSubjectLecturer.sql
@@ -0,0 +1,8 @@
+/* Students who are under a specific lecturer, under a specific subject,
+ * who achieved over a specific mark threshold (in an exam).
+ */
+
+ SELECT STUDENT.First_Name, STUDENT.Last_Name
+ FROM Lecturer l, Subject sub, Student stu, ExamResult e
+ WHERE l.First_Name="Matthew", l.Last_Name="Huntbach", sub.Name="ADSOOF"
+ 		e.Mark_Achieved=60;
--- QUERY/StudentsPassed.sql
+++ QUERY/StudentsPassed.sql
@@ -4,6 +4,6 @@
  */
 
  SELECT STUDENT.First_Name, STUDENT.Last_Name
- FROM Exam e, CourseworkResult c
+ FROM Exam e, CourseworkResult c, Student
  WHERE e.Total_Mark >= 60
  AND c.Mark_Achieved >= 40;
