0c559f800407354bd968b0cdb56fcf8102b68e75
Sameen Islam <ec14370@qmul.ac.uk>
2016-04-27 22:18:21 +0100

    Added Tom's query

---
+++ QUERY/lecWithNoStu.sql
@@ -0,0 +1,9 @@
+/* Find all lecturers appointed to a subject that has no enrolled students */
+SELECT l.LECTURER_ID as "Lecturer ID", l.TITLE as "Title", l.LAST_NAME as "Last name"
+FROM LECTURER l
+WHERE l.subject_id = (
+	SELECT subject_id 
+	FROM Subject su
+	WHERE NOT EXISTS (SELECT subject_id
+					  FROM Student st
+					  WHERE st.subject_id = su.subject_id));
---
+++ QUERY/quesAuthLec.sql
@@ -0,0 +1,12 @@
+/* Find the number of questions that was authored by the same lecturer.*/
+
+
+/* Find all lecturers appointed to a subject that has no enrolled students */
+SELECT l.LECTURER_ID as "Lecturer ID", l.TITLE as "Title", l.LAST_NAME as "Last name"
+FROM LECTURER l
+WHERE l.subject_id = (
+	SELECT subject_id 
+	FROM Subject su
+	WHERE NOT EXISTS (SELECT subject_id
+					  FROM Student st
+					  WHERE st.subject_id = su.subject_id));
