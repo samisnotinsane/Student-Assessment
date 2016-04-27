6d0bc7a5bda56decb26bf07f96e446556db34e90
Sameen Islam <ec14370@qmul.ac.uk>
2016-04-27 19:49:26 +0100

    Finished subjects with coursework query

--- QUERY/subWithCw.sql
+++ QUERY/subWithCw.sql
@@ -1,3 +1,8 @@
-/* Subjects which have courseworks. */
+/* Subjects which have courseworks. Listed in alphabetical order. */
 
 SELECT Name 
+FROM Subject s
+WHERE EXISTS (SELECT * 
+			  FROM Coursework c
+			  WHERE s.Subject_Id = c.Subject_Id)
+ORDER BY s.Name;
