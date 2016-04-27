7b90de44d726bf8f919241957b7927bcce2e626d
Sameen Islam <ec14370@qmul.ac.uk>
2016-04-27 18:38:12 +0100

    Patched sameen's new query

--- QUERY/lecQCount.sql
+++ QUERY/lecQCount.sql
@@ -1,7 +1,7 @@
 /* Show the number of questions published by each lecturer
- * grouped by exam, out of total number of questions in an exam.*/
+   grouped by exam, out of total number of questions in an exam.*/
 
- SELECT exam_id, COUNT(question_id) AS lqCount, SUM(question_id) AS (tCount)
- FROM Exam
- GROUP BY exam_id
- ORDER BY exam_id
+ SELECT e.exam_id, COUNT(q.question_id) AS lqCount, SUM(q.question_id) AS tCount
+ FROM Exam e, Question q
+ GROUP BY e.exam_id
+ ORDER BY e.exam_id;
